a4d8308f020fea1e7c27da355be1939e125ed8d2
pheonixspec <kshitijattri@gmail.com>
2026-02-16 21:39:52 +0530

    made changes after pull

---
+++ OOPSbannerstringarray.java
@@ -0,0 +1,17 @@
+public class OOPSbannerstringarray {
+
+    public static void main(String args[]){
+
+        lines[0] = System.out.println(String.join(" " , " *****  " , " *****  " , " ****** " , "  **** "));
+        lines[1] = System.out.println(String.join(" " , "*     * " , "*     * " , "*     * " , " *     "));
+        lines[2] = System.out.println(String.join(" " , "*     * " , "*     * " , "*     * " , " *     "));
+        lines[3] = System.out.println(String.join(" " , "*     * " , "*     * " , "******* " , " ***** "));
+        lines[4] = System.out.println(String.join(" " , "*     * " , "*     * " , "*       " , "     * "));
+		lines[5] = System.out.println(String.join(" " , "*     * " , "*     * " , "*       " , "     * "));
+		lines[6] = System.out.println(String.join(" " , " *****  " , " *****  " , "*       " , " ****  "));
+	
+	for (String line : lines) {
+		System.out.println(line);
+	}
+	}
+}
