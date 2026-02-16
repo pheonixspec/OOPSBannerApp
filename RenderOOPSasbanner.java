b6dddb78a8741a9c4aee4ab80a7ca8e2e4f765b9
pheonixspec <kshitijattri@gmail.com>
2026-02-16 21:24:25 +0530

    made changes

--- RenderOOPSasbanner.java
+++ RenderOOPSasbanner.java
@@ -1,10 +1,12 @@
 public class RenderOOPSasbanner {
 
     public static void main(String args[]){
-        System.out.println(" *****  " + " *****  " + " ****** " + "  *****");
+        System.out.println(" *****  " + " *****  " + " ****** " + "  **** ");
         System.out.println("*     * " + "*     * " + "*     * " + " *     ");
-        System.out.println("*     * " + "*     * " + " ****** " + "  **** ");
-        System.out.println("*     * " + "*     * " + "*       " + "      *");
-        System.out.println(" *****  " + " *****  " + "*       " + " ***** ");
+        System.out.println("*     * " + "*     * " + "*     * " + " *     ");
+        System.out.println("*     * " + "*     * " + "******* " + " ***** ");
+        System.out.println("*     * " + "*     * " + "*       " + "     * ");
+		System.out.println("*     * " + "*     * " + "*       " + "     * ");
+		System.out.println(" *****  " + " *****  " + "*       " + " ****  ");
 	}
 }
